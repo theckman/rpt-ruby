de92624e8feed4c60c84742430dfd6a25c6e8553
Tim Heckman <tim+code@timheckman.net>
2014-02-08 15:28:30 -0800

    make the Base parser rubocop compliant

--- lib/rmuh/rpt/log/parsers/base.rb
+++ lib/rmuh/rpt/log/parsers/base.rb
@@ -2,15 +2,19 @@ module RMuh
   module RPT
     module Log
       module Parsers
+        # TODO: Base Class documentation
+        #
         class Base
           def initialize
-
           end
 
           def parse(loglines)
-            raise ArgumentError, 'argument 1 must be a StringIO object' unless loglines.is_a? StringIO
-            lines = loglines.map do |line|
-              {:type => :log, :message => line}
+            fail(
+              ArgumentError, 'argument 1 must be a StringIO object'
+            ) unless loglines.is_a? StringIO
+
+            loglines.map do |line|
+              { type: :log, message: line }
             end
           end
         end
