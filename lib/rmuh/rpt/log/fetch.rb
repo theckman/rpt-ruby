cc758fea51fd091b1c4ef5b6ba37d6f7ea71733d
Tim Heckman <tim+code@timheckman.net>
2014-02-08 15:24:05 -0800

    make the Fetch class rubocop compliant

--- lib/rmuh/rpt/log/fetch.rb
+++ lib/rmuh/rpt/log/fetch.rb
@@ -4,23 +4,25 @@ require 'ostruct'
 module RMuh
   module RPT
     module Log
+      # TODO: Class documentation
+      #
       class Fetch
         include HTTParty
         attr_accessor :cfg
 
         def initialize(log_url, byte_start = 0, byte_end = nil)
-          @cfg = OpenStruct.new({
+          @cfg = OpenStruct.new(
             log_url: log_url,
             byte_start: byte_start,
             byte_end: byte_end
-          })
+          )
         end
 
         def byte_start=(bytes)
           if bytes.is_a?(Integer)
             @cfg.byte_start = bytes
           else
-            raise ArgumentError, 'argument 1 must be an integer'
+            fail ArgumentError, 'argument 1 must be an integer'
           end
         end
 
@@ -28,21 +30,21 @@ module RMuh
           if bytes.nil? || bytes.is_a?(Integer)
             @cfg.byte_end = bytes
           else
-            raise ArgumentError, 'argument 1 must be nil or an integer'
+            fail ArgumentError, 'argument 1 must be nil or an integer'
           end
         end
 
-        def get_size
+        def size
           self.class.head(@cfg.log_url).headers['content-length'].to_i
         end
 
-        def get_log
+        def log
           headers = { 'Range' => "bytes=#{@cfg.byte_start}-#{@cfg.byte_end}" }
           response = self.class.get(@cfg.log_url, headers: headers)
           StringIO.new(response.lines.map { |l| dos2unix(l) }.join)
         end
 
-        private 
+        private
 
         def dos2unix(line)
           line.gsub("\r\n", "\n")
