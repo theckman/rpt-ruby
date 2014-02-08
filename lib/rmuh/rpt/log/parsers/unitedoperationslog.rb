6f223add5a105b70fede920e879ed5817e6669fa
Tim Heckman <tim+code@timheckman.net>
2014-02-08 15:41:13 -0800

    make UnitedOperationsLog rubocop compliant

--- lib/rmuh/rpt/log/parsers/unitedoperationslog.rb
+++ lib/rmuh/rpt/log/parsers/unitedoperationslog.rb
@@ -8,25 +8,16 @@ module RMuh
   module RPT
     module Log
       module Parsers
+        # TODO: UnitedOperationsLog Class Documentation
+        #
         class UnitedOperationsLog < RMuh::RPT::Log::Parsers::Base
           include RMuh::RPT::Log::Util::UnitedOperations
           include RMuh::RPT::Log::Util::UnitedOperationsLog # Regexp Constants
 
-          def initialize(opts ={})
-            if !opts[:to_zulu].nil? && ![TrueClass, FalseClass].include?(opts[:to_zulu].class)
-              raise ArgumentError,
-                    ':to_zulu must be a boolean value (true|false)'
-            end
-
-            if !opts[:timezone].nil? && opts[:timezone].class != TZInfo::DataTimezone
-              raise ArgumentError,
-                    ':tiemzone must be an instance of TZInfo::DataTimezone'
-            end
-
-            if !opts[:chat].nil? && ![TrueClass, FalseClass].include?(opts[:chat].class)
-              raise ArgumentError,
-                    ':chat must be a boolean value (true|false)'
-            end
+          def initialize(opts = {})
+            validate_to_zulu(opts)
+            validate_timezone(opts)
+            validate_chat(opts)
 
             @incldue_chat = opts[:chat].nil? ? false : opts[:chat]
             @to_zulu = opts[:to_zulu].nil? ? true : opts[:to_zulu]
@@ -34,38 +25,68 @@ module RMuh
           end
 
           def parse(loglines)
-            if !loglines.is_a?(StringIO)
-              raise ArgumentError, 'argument 1 must be a StringIO object'
+            unless loglines.is_a?(StringIO)
+              fail ArgumentError, 'argument 1 must be a StringIO object'
             end
-
-            loglines.map do |l|
-              if GUID.match(l)
-                line = {type: :guid}.merge(m_to_h($~))
-              elsif @include_chat && CHAT.match(l)
-                line = {type: :chat}.merge(m_to_h($~))
-              else
-                line = nil
-              end
-
-              when_am_i!(line) unless line.nil?
-            end.compact
+            regex_matches(loglines)
           end
 
           private
 
+          def validate_to_zulu(opts)
+            if !opts[:to_zulu].nil? &&
+               ![TrueClass, FalseClass].include?(opts[:to_zulu].class)
+              fail ArgumentError,
+                   ':to_zulu must be a boolean value (true|false)'
+            end
+          end
+
+          def validate_timezone(opts)
+            if !opts[:timezone].nil? &&
+               opts[:timezone].class != TZInfo::DataTimezone
+              fail ArgumentError,
+                   ':tiemzone must be an instance of TZInfo::DataTimezone'
+            end
+          end
+
+          def validate_chat(opts)
+            if !opts[:chat].nil? &&
+               ![TrueClass, FalseClass].include?(opts[:chat].class)
+              fail ArgumentError,
+                   ':chat must be a boolean value (true|false)'
+            end
+          end
+
+          def regex_matches(loglines)
+            loglines.map do |l|
+              line = nil
+              if GUID.match(l)
+                line = { type: :guid }.merge(m_to_h($LAST_MATCH_INFO))
+              elsif @include_chat && CHAT.match(l)
+                line = { type: :chat }.merge(m_to_h($LAST_MATCH_INFO))
+              end
+              when_am_i!(line) unless line.nil?
+            end.compact
+          end
+
           def when_am_i!(line)
             case line[:hour]
             when 4..23
-              if Time.now.hour.between?(4, 23)
-                t = @timezone.now
-              else
-                t = @timezone.now - ONE_DAY
-              end
+              t = date_of_line_base_on_now
               set_line_date!(line, t)
             when 0..3
               set_line_date!(line)
             end
             line
+          end
+
+          def date_of_line_based_on_now
+            if Time.now.hour.between?(4, 23)
+              t = @timezone.now
+            else
+              t = @timezone.now - ONE_DAY
+            end
+            t
           end
 
           def set_line_date!(line, time = @timezone.now)
