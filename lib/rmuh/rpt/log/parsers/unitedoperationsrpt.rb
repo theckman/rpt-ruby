b079e25fa03fb3fc51ad6a5a7892cd703a44a584
Tim Heckman <tim+code@timheckman.net>
2014-02-08 15:50:16 -0800

    make UnitedOperationsRPT rubocop compliant

--- lib/rmuh/rpt/log/parsers/unitedoperationsrpt.rb
+++ lib/rmuh/rpt/log/parsers/unitedoperationsrpt.rb
@@ -2,55 +2,56 @@ require 'stringio'
 require 'tzinfo'
 
 require 'rmuh/rpt/log/parsers/base'
-require 'rmuh/rpt/log/util/unitedoperations'
+require 'rmuh/rpt/log/util/unitedoperatireons'
 require 'rmuh/rpt/log/util/unitedoperationsrpt'
 
 module RMuh
   module RPT
     module Log
       module Parsers
+        # TODO: UnitedOperationsRPT Class Documentation
+        #
         class UnitedOperationsRPT < RMuh::RPT::Log::Parsers::Base
           include RMuh::RPT::Log::Util::UnitedOperations
           include RMuh::RPT::Log::Util::UnitedOperationsRPT # Regexp Constants
 
           def initialize(opts = {})
-
-            if !opts[:to_zulu].nil? && ![TrueClass, FalseClass].include?(opts[:to_zulu].class)
-              raise ArgumentError,
-                    ':to_zulu must be a boolean value (true|false)'
-            end
-
-            if !opts[:timezone].nil? && opts[:timezone].class != TZInfo::DataTimezone
-              raise ArgumentError,
-                    ':tiemzone must be an instance of TZInfo::DataTimezone'
-            end
+            validate_opts(opts)
 
             @to_zulu = opts[:to_zulu].nil? ? true : opts[:to_zulu]
             @timezone = opts[:timezone].nil? ? UO_TZ : opts[:timezone]
           end
 
+          def validate_opts(opts)
+            validate_to_zulu(opts)
+            validate_timezone(opts)
+          end
+
           def parse(loglines)
-            if !loglines.is_a?(StringIO)
-              raise ArgumentError, 'argument 1 must be a StringIO object'
+            unless loglines.is_a?(StringIO)
+              fail ArgumentError, 'argument 1 must be a StringIO object'
             end
 
             loglines.map do |l|
-              if ANNOUNCEMENT.match(l)
-                # $~ == $LAST_MATCH_INFO (last Regexp MatchData)
-                line = {:type => :announcement}.merge(m_to_h($~))
-              elsif WOUNDED.match(l)
-                line = {:type => :wounded}.merge(m_to_h($~))
-              elsif KILLED.match(l)
-                line = {:type => :killed}.merge(m_to_h($~))
-              elsif DIED.match(l)
-                line = {:type => :died}.merge(m_to_h($~))
-              else
-                line = nil
-              end
-
-              zulu!(line, @timezone) if @to_zulu and !line.nil?
+              line = regex_match(l)
+              zulu!(line, @timezone) if @to_zulu && !line.nil?
               add_guid!(line) unless line.nil?
             end.compact
+          end
+
+          private
+
+          def regex_match(l)
+            if ANNOUNCEMENT.match(l)
+              line = { type: :announcement }.merge(m_to_h($LAST_MATCH_INFO))
+            elsif WOUNDED.match(l)
+              line = { type: :wounded }.merge(m_to_h($LAST_MATCH_INFO))
+            elsif KILLED.match(l)
+              line = { type: :killed }.merge(m_to_h($LAST_MATCH_INFO))
+            elsif DIED.match(l)
+              line = { type: :died }.merge(m_to_h($LAST_MATCH_INFO))
+            end
+            (defined?(line)) ? line : nil
           end
         end
       end
