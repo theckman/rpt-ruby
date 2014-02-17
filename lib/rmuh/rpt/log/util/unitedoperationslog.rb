b67e563e4f1bab12b7dacf03816609a2b8976536
Tim Heckman <tim+code@timheckman.net>
2014-02-16 16:30:43 -0800

    make the UnitedOperationsLog util rubocop compliant

--- lib/rmuh/rpt/log/util/unitedoperationslog.rb
+++ lib/rmuh/rpt/log/util/unitedoperationslog.rb
@@ -2,11 +2,22 @@ module RMuh
   module RPT
     module Log
       module Util
+        # The utility module for the UnitedOperationsLog parser
+        # This provides the constants needed for Regex matches
+        # as well as the validate_chat function
+        #
         module UnitedOperationsLog
-          ONE_DAY ||= 86400
-          TIME ||= '^\s*?(?<hour>\d+?):(?<min>\d+?):(?<sec>\d+)\sBattlEye\sServer:\s'
-          GUID ||= %r{#{TIME}.*Verified\sGUID\s\((?<player_beguid>.*?)\).*#\d+?\s(?<player>.+)$}
-          CHAT ||= %r{#{TIME}\((?<channel>Group|Global|Side|Vehicle|Command|Unknown)\)\s+?(?<player>.+?):\s(?<message>.*)$}
+          ONE_DAY ||= 86_400
+          TIME ||= '^\s*?(?<hour>\d+?):(?<min>\d+?):(?<sec>\d+)\s' \
+                   'BattlEye\sServer:\s'
+          GUID ||= Regexp.new(
+            "#{TIME}.*Verified\\sGUID\\s\\((?<player_beguid>.*?)\\).*#\\d+?" \
+            '\s(?<player>.+)$'
+          )
+          CHAT ||= Regexp.new(
+            "#{TIME}\\((?<channel>Group|Global|Side|Vehicle|Command|Unknown)" \
+            '\)\s+?(?<player>.+?):\s(?<message>.*)$'
+          )
 
           def validate_chat(opts)
             if !opts[:chat].nil? &&
