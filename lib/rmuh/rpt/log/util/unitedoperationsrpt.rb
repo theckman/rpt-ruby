aa72806cd6274ee999ba1fb71bd8a16a4f143eea
Tim Heckman <tim+code@timheckman.net>
2014-02-16 17:36:03 -0800

    make RMuh::RPT::Log::Util::UnitedOperationsRPT rubocop compliant

--- lib/rmuh/rpt/log/util/unitedoperationsrpt.rb
+++ lib/rmuh/rpt/log/util/unitedoperationsrpt.rb
@@ -2,14 +2,33 @@ module RMuh
   module RPT
     module Log
       module Util
+        # The utility module for the UnitedOperationsRPT parser
+        # This provides the constants for Regexp matches
+        #
         module UnitedOperationsRPT
-
-          DTR ||= '(?<year>\d+)/(?<month>\d+)/(?<day>\d+),\s+?(?<hour>\d+):(?<min>\d+):(?<sec>\d+)'
-          KILLED ||= %r{^#{DTR}\s"(?<server_time>[0-9.]+).*?:\s(?<victim>.*?)\s\((?<victim_team>.*?)\)\s.*?by\s(?<offender>.*?)\s\((?<offender_team>.*?)\).*?position: \[(?<victim_position>.*?)\].*?GRID (?<victim_grid>\d+)\).*?position: \[(?<offender_position>.*?)\].*?GRID (?<offender_grid>\d*)\).*?:\s(?<distance>[0-9e.+]+).*?(?:(?<nearby_players>None.|\[.*?\])")}
-          DIED ||= %r{^#{DTR}\s"(?<server_time>[0-9.]+).*?:\s(?<victim>.*?) has died at \[(?<victim_position>.*?)\].*?GRID (?<victim_grid>\d+)\).*?(?:(?<nearby_players>None.|\[.*?\])")}
-          WOUNDED ||= %r{^#{DTR}\s"(?<server_time>[0-9.]+).*?:\s(?<victim>.*?)\s\((?<victim_team>.*?)\)\s.*?by\s(?<offender>.*?)\s\((?<offender_team>.*?)\).*?(?<damage>[0-9.]+)\sdamage}
-          ANNOUNCEMENT ||= %r{^#{DTR}\s"(?<head>[#]+?)\s(?<message>.*?)\s(?<tail>[#]+?)"}
-
+          DTR ||= '(?<year>\d+)/(?<month>\d+)/(?<day>\d+),\s+?(?<hour>\d+):' \
+                  '(?<min>\d+):(?<sec>\d+)'
+          KILLED ||= Regexp.new(
+            "^#{DTR}\\s\"(?<server_time>[0-9.]+).*?:\\s(?<victim>.*?)\\s\\(" \
+            '(?<victim_team>.*?)\)\s.*?by\s(?<offender>.*?)\s\(' \
+            '(?<offender_team>.*?)\).*?position: \[(?<victim_position>.*?)\]' \
+            '.*?GRID (?<victim_grid>\d+)\).*?position: \[' \
+            '(?<offender_position>.*?)\].*?GRID (?<offender_grid>\d*)\).*?:' \
+            '\s(?<distance>[0-9e.+]+).*?(?:(?<nearby_players>None.|\[.*?\])")'
+          )
+          DIED ||= Regexp.new(
+            "^#{DTR}\\s\"(?<server_time>[0-9.]+).*?:\\s(?<victim>.*?) " \
+            'has died at \[(?<victim_position>.*?)\].*?GRID ' \
+            '(?<victim_grid>\d+)\).*?(?:(?<nearby_players>None.|\[.*?\])")'
+          )
+          WOUNDED ||= Regexp.new(
+            "^#{DTR}\\s\"(?<server_time>[0-9.]+).*?:\\s(?<victim>.*?)\\s" \
+            '\((?<victim_team>.*?)\)\s.*?by\s(?<offender>.*?)\s\(' \
+            '(?<offender_team>.*?)\).*?(?<damage>[0-9.]+)\sdamage'
+          )
+          ANNOUNCEMENT ||= Regexp.new(
+            "^#{DTR}\\s\"(?<head>[#]+?)\s(?<message>.*?)\s(?<tail>[#]+?)\""
+          )
         end
       end
     end
