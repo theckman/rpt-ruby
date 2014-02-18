44ba57589849186d1bc5f79631f8cef4ec674fc5
Tim Heckman <tim+code@timheckman.net>
2014-02-18 03:07:48 -0800

    oh yeah, should probably bump version

--- lib/rmuh/rpt.rb
+++ lib/rmuh/rpt.rb
@@ -1,8 +1,10 @@
 module RMuh
+  # This is the RPT module of RMuh. This file just provides version info
+  #
   module RPT
     V_MAJ = 0
     V_MIN = 0
-    V_REV = 2
+    V_REV = 5
     VERSION = "#{V_MAJ}.#{V_MIN}.#{V_REV}"
   end
 end
