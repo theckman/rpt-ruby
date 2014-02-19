af69ff1bc42b067fb3b0021f869705edd272fb4d
Tim Heckman <tim+code@timheckman.net>
2014-02-18 19:34:07 -0800

    fix version-related specs

--- spec/rmuh_rpt_spec.rb
+++ spec/rmuh_rpt_spec.rb
@@ -1,44 +1,44 @@
 require 'rspec'
-require File.join(File.expand_path('../..', __FILE__), 'lib/rmuh/rpt')
+require File.join(File.expand_path('../..', __FILE__), 'lib/rmuh/version')
 
-describe RMuh::RPT do
-  context '::V_MAJ' do
+describe RMuh do
+  context '::VERSION_MAJ' do
     it 'should have a major version that is an integer' do
-      RMuh::RPT::V_MAJ.is_a?(Integer).should be_true
+      RMuh::VERSION_MAJ.is_a?(Integer).should be_true
     end
 
     it 'should have a major version that is a positive number' do
-      (RMuh::RPT::V_MAJ > -1).should be_true
+      (RMuh::VERSION_MAJ > -1).should be_true
     end
   end
 
-  context '::V_MIN' do
+  context '::VERSION_MIN' do
     it 'should have a minor version that is an integer' do
-      RMuh::RPT::V_MIN.is_a?(Integer).should be_true
+      RMuh::VERSION_MIN.is_a?(Integer).should be_true
     end
 
     it 'should have a minor version that is a positive integer' do
-      (RMuh::RPT::V_MIN > -1).should be_true
+      (RMuh::VERSION_MIN > -1).should be_true
     end
   end
 
-  context '::V_REV' do
+  context '::VERSION_REV' do
     it 'should have a revision number that is an integer' do
-      RMuh::RPT::V_REV.is_a?(Integer).should be_true
+      RMuh::VERSION_REV.is_a?(Integer).should be_true
     end
 
     it 'should have a revision number that is a positive integer' do
-      (RMuh::RPT::V_REV > -1).should be_true
+      (RMuh::VERSION_REV > -1).should be_true
     end
   end
 
   context '::VERSION' do
     it 'should have a version that is a string' do
-      RMuh::RPT::VERSION.should be_an_instance_of String
+      RMuh::VERSION.should be_an_instance_of String
     end
 
     it 'should match the following format N.N.N' do
-      /\A(?:\d+?\.){2}\d+?/.match(RMuh::RPT::VERSION).should_not be_nil
+      /\A(?:\d+?\.){2}\d+?/.match(RMuh::VERSION).should_not be_nil
     end
   end
 end
