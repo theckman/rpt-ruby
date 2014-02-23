7c4af36694cbd1a47cad74eb2e7120b89de17b70
Tim Heckman <tim+code@timheckman.net>
2014-02-22 18:39:18 -0800

    bump version to 0.1.0

--- lib/rmuh/version.rb
+++ lib/rmuh/version.rb
@@ -2,8 +2,8 @@
 #
 module RMuh
   VERSION_MAJ ||= 0
-  VERSION_MIN ||= 0
-  VERSION_REV ||= 7
+  VERSION_MIN ||= 1
+  VERSION_REV ||= 0
 
   VERSION ||= "#{VERSION_MAJ}.#{VERSION_MIN}.#{VERSION_REV}"
 end
